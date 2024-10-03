fcd413854f71b066f2c9789d9ebb321f13aa8588
Cary2332 <anacarielesilva12e@gmail.com>
2024-10-03 11:41:12 -0300

    Add files via upload

---
+++ bet/src/main/java/snippet/Snippet.java
@@ -0,0 +1,13 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		
+		
+		this.cpf = user.cpf();
+		this.niver = user.niver();
+		this.email = user.email();
+		this.senha = user.senha();
+	}
+}
+
